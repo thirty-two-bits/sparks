502af0b6509ad9c7791ea2f87478e9b421a26351
Alex Kessinger <voidfiles@gmail.com>
2014-12-17 15:18:55 -0800

    fix tests

--- catalog/tests/test_api.py
+++ catalog/tests/test_api.py
@@ -10,14 +10,14 @@ class TestApi(MockModel, TestCase):
         self.setup_basic_system()
 
     def test_cors(self):
-        response = self.client.options('/api/admin/stats/', HTTP_ORIGIN='http://localhost:9000', HTTP_ACCESS_CONTROL_REQUEST_METHOD='GET')
+        response = self.client.options('/api/admin/stats', HTTP_ORIGIN='http://localhost:9000', HTTP_ACCESS_CONTROL_REQUEST_METHOD='GET')
         assert response.status_code == 200
         assert response['Access-Control-Allow-Origin'] == 'http://localhost:9000'
-        response = self.client.get('/api/admin/stats/', HTTP_ORIGIN='http://localhost:9000')
+        response = self.client.get('/api/admin/stats', HTTP_ORIGIN='http://localhost:9000')
         assert response['Access-Control-Allow-Origin'] == 'http://localhost:9000'
 
     def test_admin_stats_endpoint(self):
-        response = self.client.get('/api/admin/stats/')
+        response = self.client.get('/api/admin/stats')
         self.assertEqual(response.status_code, 200)
         resp = json.loads(response.content)
         self.assertEquals(resp['total_sources'], 1)
@@ -31,7 +31,7 @@ class TestApi(MockModel, TestCase):
         self.assertEquals(resp['total_origins'], 1)
 
     def test_article_view(self):
-        response = self.client.get('/api/articles/')
+        response = self.client.get('/api/articles')
         self.assertEqual(response.status_code, 200)
         resp = json.loads(response.content)
         assert len(resp) == 1
