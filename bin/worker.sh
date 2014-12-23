db8acd04ffcf912dc0580d6e9dfe90ab7d3b708d
Alex Kessinger <voidfiles@gmail.com>
2014-12-23 00:14:35 -0800

    Fix permissions

--- bin/worker.sh
+++ bin/worker.sh
@@ -1,3 +1,2 @@
 python manage.py celery worker --loglevel=INFO -Q celery -n worker2.%h -c 3 --beat --time-limit=300 &
 python manage.py celery worker --loglevel=INFO -Q http -n worker1.%h -c 20 -P eventlet --time-limit=300
-
